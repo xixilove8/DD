e416a183e898e45350b401ef61ef746ca2098ba6
xixilove8 <89375952+xixilove8@users.noreply.github.com>
2022-10-17 19:14:44 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -28,7 +28,11 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //助力好友分享码(最多3个,否则后面的助力失败)
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes = ['']
+let shareCodes = ['r4uf7wafbajmiemfmjjcoqr2kveedgn7nyubrei@bigy6bioqo4fz7ncmnfvkn7d7y@xfvsxz3wd6evwexpy7d3c7vqtpx4j4rnnu4wm5q@nkghpcy64kthfmjy4wyg3d3vzeqxtevyukjzwuy@gcdr655xfdjq7riepzkmql424kdfoakqowtycga'
+		 'r4uf7wafbajmiemfmjjcoqr2kveedgn7nyubrei@bigy6bioqo4fz7ncmnfvkn7d7y@xfvsxz3wd6evwexpy7d3c7vqtpx4j4rnnu4wm5q@nkghpcy64kthfmjy4wyg3d3vzeqxtevyukjzwuy@gcdr655xfdjq7riepzkmql424kdfoakqowtycga'
+		 'r4uf7wafbajmiemfmjjcoqr2kveedgn7nyubrei@bigy6bioqo4fz7ncmnfvkn7d7y@xfvsxz3wd6evwexpy7d3c7vqtpx4j4rnnu4wm5q@nkghpcy64kthfmjy4wyg3d3vzeqxtevyukjzwuy@gcdr655xfdjq7riepzkmql424kdfoakqowtycga'
+		 'r4uf7wafbajmiemfmjjcoqr2kveedgn7nyubrei@bigy6bioqo4fz7ncmnfvkn7d7y@xfvsxz3wd6evwexpy7d3c7vqtpx4j4rnnu4wm5q@nkghpcy64kthfmjy4wyg3d3vzeqxtevyukjzwuy@gcdr655xfdjq7riepzkmql424kdfoakqowtycga'
+		 'r4uf7wafbajmiemfmjjcoqr2kveedgn7nyubrei@bigy6bioqo4fz7ncmnfvkn7d7y@xfvsxz3wd6evwexpy7d3c7vqtpx4j4rnnu4wm5q@nkghpcy64kthfmjy4wyg3d3vzeqxtevyukjzwuy@gcdr655xfdjq7riepzkmql424kdfoakqowtycga']
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
